eb55b6f0b035039715c913f6e45c47e175246d87
MSM Shahla <maryamshahla465@gmail.com>
2026-04-15 17:18:08 +0530

    Added Main class to run the API server

---
+++ src/main/java/com/smartcampus/Main.java
@@ -0,0 +1,30 @@
+package com.smartcampus;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class Main {
+
+    // Updated base URI (includes /api/v1)
+    public static final String BASE_URI = "http://localhost:8080/api/v1/";
+
+    public static HttpServer startServer() {
+        final ResourceConfig rc = new ResourceConfig();
+
+        // Manually register your resource
+        rc.register(com.smartcampus.resources.HelloResource.class);
+
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
+    public static void main(String[] args) throws IOException {
+        final HttpServer server = startServer();
+        System.out.println("Server running at " + BASE_URI);
+        System.in.read(); // keeps server running
+        server.shutdownNow();
+    }
+}
